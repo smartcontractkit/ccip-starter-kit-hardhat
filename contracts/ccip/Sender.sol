668975626efa0e3c11b685058d63b6f417c87cb8
luckyswaminathan <lakshman.swaminathan1@gmail.com>
2024-02-22 19:44:58 -0500

    structure:

---
+++ contracts/ccip/Sender.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Sender {
+   
+
+
+
+    uint256 public fee;
+
+
+    event Sender {
+        address i_router;
+        address i_link;
+    }
+
+
+
+
+
+
+
+    constructor() {
+        // Initialize state variables here
+    }
+}
